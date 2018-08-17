d904ad060af74caec089a929a8b7ba6d01eeda52
hodaya55 <hodayah159@gmail.com>
2018-08-17 17:51:44 +0300

    update

--- client/src/components/content/mapView/MapView.js
+++ client/src/components/content/mapView/MapView.js
@@ -3,11 +3,26 @@ import Map from './map/Map';
 import Location from './location/Location';
 
 export default class MapView extends Component {
+  state = {
+    address: {
+      lat: '', lng: ''
+    }
+  }
+
+  // address = {
+  //   lat: '', lng: ''
+  // }
+  updateAddress = (address) => {
+    console.log(address);
+    this.setState({ address: address });
+    // this.address = address;
+  }
   render() {
     return (
       <div>
-        <Location />
-        <Map />
+        <Location updateAddress={this.updateAddress} />
+        {/* <Map address={this.address} /> */}
+        <Map address={this.state.address} />
       </div>
     )
   }
