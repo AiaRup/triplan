68379622e06ccb9596af754148444da5109fb818
AiaRup <aiale88@gmail.com>
2018-08-19 20:25:03 +0300

    merge server and api

--- api/app.js
+++ api/app.js
@@ -1,18 +1,34 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const ObjectID = require('mongodb').ObjectID;
+const request = require('request');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const User = require('./models/userModel');
+const Plan = require('./models/planModel').plan;
+
+mongoose.Promise = global.Promise;
+// Connect to DB and check the connection
+const connection = process.env.CONNECTION_STRING || 'mongodb://localhost:27017/triplanDB';
+mongoose.connect(connection, { useNewUrlParser: true })
+  .then(() => { console.log('Successfully connected to mongoDB'); })
+  .catch(error => console.error(error));
+
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,6 +36,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', indexRouter);
 app.use('/api/users', usersRouter);
+// app.use(express.static('public'));
+app.use(express.static('node_modules'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 
 // catch 404 and forward to error handler
