588f63bda0276e1d531659795e9b68329f814274
AiaRup <aiale88@gmail.com>
2018-08-21 19:27:48 +0300

    layout map component

--- client/src/components/content/mapView/map/Map.js
+++ client/src/components/content/mapView/map/Map.js
@@ -2,85 +2,14 @@ import React, { Component } from 'react';
 import './Map.css';
 import axios from 'axios';
 import _ from 'lodash';
-import { StandaloneSearchBox } from 'react-google-maps/lib/components/places/StandaloneSearchBox';
-import { compose, withProps, withState, lifecycle, withHandlers } from 'recompose';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import GoogleMap from '../googleMap/GoogleMap';
+
 // import 'map-icons/dist/fonts';
 // import 'map-icons/dist/css/map-icons.css';
 // import 'map-icons/dist/js/map-icons.js';
 // const google = window.google;
 
-const MyMapComponent = compose(
-  withState({ zoom: 12 }),
-  withProps({
-    googleMapURL: 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&language=en&key=AIzaSyDuKj7l762Y5ulcwj_EyANIvHx6rfffceY',
-    loadingElement: <div style={{ height: '100%' }} />,
-    containerElement: <div style={{ height: '100%' }} />,
-    mapElement: <div style={{ height: '100%' }} />,
-    isMarkerShown: true,
-    zoom: 14,
-  }),
-  lifecycle({
-    componentWillMount() {
-      const refs = {};
-      this.setState({
-        onSearchBoxMounted: ref => {
-          refs.searchBox = ref;
-        },
-        onPlacesChanged: () => {
-          const place = refs.searchBox.getPlaces();
-          console.log('place');
-          console.log(place);
-          let lat = place[0].geometry.location.lat();
-          let lng = place[0].geometry.location.lng();
-          this.myLocation = { lat: lat, lng: lng };
-          this.nameLocation = place[0].formatted_address;
-          console.log(this.myLocation);
-          this.props.updateAddress(this.myLocation);
-          this.setState({ zoom: 13 });
-        },
-        onMarkerClick: () => {
-          console.log('marker clicked!');
-          console.log(this.props.zoom);
-          console.log(this.state.zoom);
-        }
-      });
-    }, // end componentWillMount
-
-  }),
-  withScriptjs,
-  withGoogleMap
-)((props) => {
-  console.log('in map props- markers:', props.markers);
-
-  return <div>
-    <StandaloneSearchBox
-      ref={props.onSearchBoxMounted}
-      bounds={props.bounds}
-      onPlacesChanged={props.onPlacesChanged}>
-      <input
-        type="text"
-        placeholder="Search Address"
-        className="autocomplete"/>
-    </StandaloneSearchBox>
-    <GoogleMap
-      defaultZoom={10}
-      zoom={props.zoom}
-      center={{ lat: props.address.lat, lng: props.address.lng }}
-      defaultCenter={{ lat: props.address.lat, lng: props.address.lng }}
-      defaultOptions={{
-        mapTypeControl: false,
-        rotateControl: false,
-        fullscreenControl: false,
-        scrollwheel: false
-      }}>
-      {props.isMarkerShown && <Marker position={{ lat: props.address.lat, lng: props.address.lng }} onClick={props.onMarkerClick} />}
-      {props.isMarkerShown && props.markers.map((marker, index) => <Marker key={index} position={{ lat: marker.position.lat, lng: marker.position.lng }} onClick={props.onMarkerClick} />)}
-    </GoogleMap>
-  </div>;
-});
-
-class MyFancyComponent extends React.PureComponent {
+class Map extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -94,12 +23,11 @@ class MyFancyComponent extends React.PureComponent {
     const promises = [];
     this.state.places.forEach((element) => {
       let type = element.type;
-
       const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${this.props.address.lat},${this.props.address.lng}&radius=1500&type=${type}&language=en&key=AIzaSyDuKj7l762Y5ulcwj_EyANIvHx6rfffceY`;
+
 
       let promise = axios(url)
         .then((response) => {
-          console.log('res', response.data.results);
           // add markers on the map
           response.data.results.forEach((location) => {
             const objLatLng = location.geometry.location;
@@ -109,21 +37,17 @@ class MyFancyComponent extends React.PureComponent {
             };
             markerArray.push(marker);
           });
-          console.log('markers array', markerArray);
         })
         .catch(function (error) {
           console.log(error);
         });
-
       promises.push(promise);
     });
-    Promise.all(promises).then(this.setState({ markers: markerArray }));
+    Promise.all(promises).then(() => {this.setState({ markers: markerArray });} );
   }
 
   isArrayEqual= (array1, array2) => {
-    let result = _(array1).differenceWith(array2, _.isEqual).isEmpty();
-    console.log('equal', result);
-    return result;
+    return _(array1).differenceWith(array2, _.isEqual).isEmpty();
   }
 
   componentDidUpdate(prevProps) {
@@ -132,34 +56,15 @@ class MyFancyComponent extends React.PureComponent {
     }
   }
 
-  // componentDidMount() {
-  //   this.delayedShowMarker();
-  // }
-
-  // delayedShowMarker = () => {
-  //   setTimeout(() => {
-  //     this.setState({ isMarkerShown: true })
-  //   }, 3000);
-  // }
-
-  // handleMarkerClick = () => {
-  //   this.setState({ isMarkerShown: false });
-  //   this.delayedShowMarker();
-  // }
-
   render() {
-    console.log('state markers', this.state.markers);
-
     return (
-      <MyMapComponent
-        onMarkerClick={this.handleMarkerClick}
+      <GoogleMap
         markers={this.state.markers}
         address={this.props.address}
-        updateAddress={this.props.updateAddress}
-      />
+        updateAddress={this.props.updateAddress}/>
     );
   }
 }
 
-export default MyFancyComponent;
+export default Map;
 
