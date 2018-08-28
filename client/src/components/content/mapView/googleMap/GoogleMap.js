94e9c5568be0d1a8a7f128c5abab7d80809f5f6f
AiaRup <aiale88@gmail.com>
2018-08-28 14:53:21 +0300

    add infoBox on hover

--- client/src/components/content/mapView/googleMap/GoogleMap.js
+++ client/src/components/content/mapView/googleMap/GoogleMap.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-undef */
 
 import React from 'react';
-// import { StandaloneSearchBox } from 'react-google-maps/lib/components/places/StandaloneSearchBox';
 import { SearchBox } from 'react-google-maps/lib/components/places/SearchBox';
 import { MarkerWithLabel } from 'react-google-maps/lib/components/addons/MarkerWithLabel';
+import { InfoBox } from 'react-google-maps/lib/components/addons/InfoBox';
+
 import { compose, withProps, lifecycle, withStateHandlers } from 'recompose';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
 
@@ -11,14 +12,24 @@ const MapComponent = compose(
   withStateHandlers(() =>
     ({
       isOpen: false,
-      infoIndex: null
+      infoIndex: null,
+      isOpenHover: false,
+      infoIndexHover: null,
+      visible: false
     }),
   {
     showInfo: ({ isOpen, infoIndex }) => (index) => ({
       isOpen: infoIndex !== index || !isOpen,
       infoIndex: index
+    }),
+    onHoverBox: ({ isOpenHover, infoIndexHover }) => (index) => ({
+      isOpenHover: infoIndexHover !== index || !isOpenHover,
+      infoIndexHover: index
+    }),
+    handleSlider: ({ visible }) => () => ({
+      visible: !visible
     })
-  }
+  },
   ),
   withProps({
     googleMapURL: 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&language=en&key=AIzaSyDuKj7l762Y5ulcwj_EyANIvHx6rfffceY',
@@ -54,17 +65,10 @@ const MapComponent = compose(
   withGoogleMap
 )((props) => {
   return <div>
-    {/* <StandaloneSearchBox
-      ref={props.onSearchBoxMounted}
-      bounds={props.bounds}
-      onPlacesChanged={props.onPlacesChanged}>
-      <input placeholder="Search Address" className="autocomplete" />
-    </StandaloneSearchBox> */}
-
     <GoogleMap
-      defaultZoom={14}
-      zoom={14}
-      center={{ lat: props.address.lat, lng: props.address.lng }}
+      defaultZoom={13}
+      zoom={13}
+      // center={{ lat: props.address.lat, lng: props.address.lng }}
       defaultCenter={{ lat: props.address.lat, lng: props.address.lng }}
       defaultOptions={{ mapTypeControl: false, rotateControl: false, scrollwheel: false }}>
 
@@ -79,26 +83,6 @@ const MapComponent = compose(
           className="autocomplete" />
       </SearchBox>
 
-      {/* <StandaloneSearchBox
-        ref={props.onSearchBoxMounted}
-        bounds={props.bounds}
-        onPlacesChanged={props.onPlacesChanged}>
-        <input placeholder="Search Address" className="autocomplete" />
-      </StandaloneSearchBox> */}
-
-      {/* <Marker position={{ lat: props.address.lat, lng: props.address.lng }}
-        defaultAnimation= {google.maps.Animation.DROP}
-        label= 'You are here'
-        icon={{
-          // url: require('./../../../../markersIcons/home.png'),
-          // Animation: google.maps.Animation.DROP,
-
-          // path: google.maps.SymbolPath.CIRCLE,
-          // scale: 8
-        }}
-      // onClick={props.onMarkerClick}
-      /> */}
-
       <MarkerWithLabel
         position={{ lat: props.address.lat, lng: props.address.lng }}
         labelAnchor={new google.maps.Point(0, 0)}
@@ -112,18 +96,43 @@ const MapComponent = compose(
           icon={{
             url: require(`../../../../markersIcons/${marker.icon}`)
           }}
+          onMouseOver={() => props.onHoverBox(marker.id)}
+          onMouseOut={() => props.onHoverBox(marker.id)}
         >
           {(props.isOpen && props.infoIndex === marker.id) &&
               <InfoWindow onCloseClick={props.showInfo}>
-                <div>
-                  <p><b>{marker.name}</b></p>
-                  <p>Rating: {marker.rating ? marker.rating : '---'}</p>
-                  <p>{marker.openNow ? 'Open Now' : 'Close Now'}</p>
-                  {/* todo: fix the website link , add the photo */}
-                  <a href={marker.website} target="_blank">Website</a>
-                  <button onClick={() => props.addPlace(marker)}>Add</button>
+                <div className="info-window">
+                  <div className='info-content'>
+                    <p className='info-header'>{marker.name}</p>
+                    <p>{marker.address}</p>
+                    <p>{marker.phone}</p>
+                    <p>{marker.price}</p>
+                    <p>Rating: {marker.rating ? marker.rating : '---'}</p>
+                    <p>{marker.openNow ? 'Open Now' : 'Close Now'}</p>
+                    <button type="button" onClick={props.handleSlider}>
+                      {props.visible ? 'Hide Opning Hours' : 'Show Opning Hours'}
+                    </button>
+                    <SliderHours className='slider-hours' visible={props.visible} openHours={marker.openHours}>
+                    </SliderHours>
+                    {marker.website ? <a href={marker.website} target="_blank">Website</a> : null}
+                    <button className='btn btn-sm btn-outline-secondary ml-3' onClick={() => props.addPlace(marker)}>Add</button>
+                  </div>
+                  <div>
+                    {marker.photo ? <img src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=130&maxheight=130&photoreference=${marker.photo}&key=AIzaSyDuKj7l762Y5ulcwj_EyANIvHx6rfffceY`} /> : null}
+                  </div>
                 </div>
               </InfoWindow>}
+
+          {(props.isOpenHover && props.infoIndexHover === marker.id) && <InfoBox
+            // onCloseClick={props.onHoverBox}
+            options={{ closeBoxURL: '', enableEventPropagation: true }}
+          >
+            <div className='info-box'>
+              <p className='info-header'><b>{marker.name}</b></p>
+              <p>{marker.address}</p>
+              <p>{marker.phone}</p>
+            </div>
+          </InfoBox>}
         </Marker>)
       }
     </GoogleMap>
@@ -131,3 +140,10 @@ const MapComponent = compose(
 });
 
 export default MapComponent;
+
+const SliderHours = ({ visible, openHours }) => (
+  <div id="slider-hours" className={visible ? 'slideIn' : 'slideOut'}>
+    {openHours ? openHours.map((day, index) => <p key={index}>{day}</p>) : null}
+
+  </div>
+);
