19732c18473c90ae4489bd8fd0ded1eb783eedb8
AiaRup <aiale88@gmail.com>
2018-08-29 22:15:38 +0300

    add message login and clear

--- client/src/components/content/mapView/googleMap/GoogleMap.js
+++ client/src/components/content/mapView/googleMap/GoogleMap.js
@@ -17,20 +17,20 @@ const MapComponent = compose(
       toggleCollapse: false,
       indexCollapse: null
     }),
-    {
-      showInfo: ({ isOpen, infoIndex }) => (index) => ({
-        isOpen: infoIndex !== index || !isOpen,
-        infoIndex: index
-      }),
-      onHoverBox: ({ isOpenHover, infoIndexHover }) => (index) => ({
-        isOpenHover: infoIndexHover !== index || !isOpenHover,
-        infoIndexHover: index
-      }),
-      collapseToggle: ({ toggleCollapse, indexCollapse }) => (index) => ({
-        toggleCollapse: indexCollapse !== index || !toggleCollapse,
-        indexCollapse: index
-      })
-    },
+  {
+    showInfo: ({ isOpen, infoIndex }) => (index) => ({
+      isOpen: infoIndex !== index || !isOpen,
+      infoIndex: index
+    }),
+    onHoverBox: ({ isOpenHover, infoIndexHover }) => (index) => ({
+      isOpenHover: infoIndexHover !== index || !isOpenHover,
+      infoIndexHover: index
+    }),
+    collapseToggle: ({ toggleCollapse, indexCollapse }) => (index) => ({
+      toggleCollapse: indexCollapse !== index || !toggleCollapse,
+      indexCollapse: index
+    })
+  },
   ),
   withProps({
     googleMapURL: 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&language=en&key=AIzaSyAewucBzhp4DIePd6P0JHbpkQ4JtPzCShE',
@@ -55,7 +55,7 @@ const MapComponent = compose(
           this.setState({
             bounds: refs.map.getBounds(),
             center: refs.map.getCenter(),
-          })
+          });
         },
         onPlacesChanged: () => {
           const place = refs.searchBox.getPlaces();
@@ -64,11 +64,11 @@ const MapComponent = compose(
           // console.log('latlng ', { lat: lat, lng: lng });
           const bounds = new google.maps.LatLngBounds();
           if (place[0].geometry.viewport) {
-            bounds.union(place[0].geometry.viewport)
+            bounds.union(place[0].geometry.viewport);
           } else {
-            bounds.extend(place[0].geometry.location)
+            bounds.extend(place[0].geometry.location);
           }
-          const nextCenter = _.get([{ position: { lat: lat, lng: lng } }], '0.position', this.state.center);
+          const nextCenter = _.get([{ position: { lat: lat, lng: lng }}], '0.position', this.state.center);
           this.setState({
             center: nextCenter,
             marker: place[0].geometry.location,
