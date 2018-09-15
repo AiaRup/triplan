be6df0aae9fcf8805c22837138a5dc8eb17856af
hodaya55 <hodayah159@gmail.com>
2018-09-16 00:29:38 +0300

    update events list style

--- client/src/components/myTrips/Card.js
+++ client/src/components/myTrips/Card.js
@@ -8,14 +8,23 @@ import DeleteIcon from '@material-ui/icons/DeleteForeverTwoTone';
 
 
 const Card = (props) => {
+  const handleDeleteTrip = () => {
+    console.log('delete trip id: ', props.plan._id);
 
+  }
   return (
     <div className="card">
+      <div className="icons-container">
+        <EditIcon />
+        <span onClick={handleDeleteTrip}>
+          <DeleteIcon>
+          </DeleteIcon>
+        </span>
+      </div>
       <Link className="watch" to={`MyTrips/${props.plan._id}`} >
-        <div className="icons-container"><EditIcon/><DeleteIcon></DeleteIcon></div>
         <h3>{props.plan.name[0].toUpperCase() + props.plan.name.slice(1)}</h3>
-        <span style={{ marginRight:'10px' }}><MarkerIcon/> {props.plan.city}, </span>
-        <span><DayIcon/> {props.plan.days.length} Days</span>
+        <span style={{ marginRight: '10px' }}><MarkerIcon /> {props.plan.city}, </span>
+        <span><DayIcon /> {props.plan.days.length} Days</span>
       </Link>
     </div>
   );
