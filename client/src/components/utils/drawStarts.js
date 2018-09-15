9fe9dde0b0ab776104d5b5a821fea5e9a7681493
AiaRup <aiale88@gmail.com>
2018-09-15 18:44:40 +0300

    add event details and address, design day plan and event dragable with icons and animation to event added

--- client/src/components/utils/drawStarts.js
+++ client/src/components/utils/drawStarts.js
@@ -7,14 +7,14 @@ export default function drawStarts(stars) {
   const starsToDraw = [];
   if (stars % 1 === 0) {
     for (let i=0 ; i < stars; i++) {
-      starsToDraw.push(<StarIcon/>);
+      starsToDraw.push(<StarIcon key={i}/>);
     }
   }
   else {
     for (let i=0 ; i < Math.floor(stars); i++) {
-      starsToDraw.push(<StarIcon/>);
+      starsToDraw.push(<StarIcon key={i}/>);
     }
-    starsToDraw.push(<StarHalfIcon/>);
+    starsToDraw.push(<StarHalfIcon key={Math.floor(stars)}/>);
   }
   return starsToDraw;
 }
